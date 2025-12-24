92c66ade3fc78729e127702e4e8cc2a4d72e72a6
kmtoki <higumaido@gmail.com>
2025-12-24 19:31:04 +0900

    blog.html re-design

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,9 +2,13 @@ export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("public");
 
- eleventyConfig.addFilter("getTagNames", collections => {
-    return Object.keys(collections);
-      //.filter(tag => !["all"].includes(tag));
+  eleventyConfig.addFilter("getTagNames", collections => {
+    if (collections == null || collections == undefined) {
+      return ["無し"]
+    } else {
+     return Object.keys(collections);
+       //.filter(tag => !["all"].includes(tag));
+    }
   });
 }
 
